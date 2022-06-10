a4ca5d6d7d88f76b547f894a18cd1a516eb81b59
pilaraf <pilaralvarezfournier@gmail.com>
2022-06-10 20:12:41 +0200

    add json file

--- src/main/java/com/ironhack/stadiumhunterapi/StadiumHunterApiApplication.java
+++ src/main/java/com/ironhack/stadiumhunterapi/StadiumHunterApiApplication.java
@@ -1,10 +1,13 @@
 package com.ironhack.stadiumhunterapi;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ironhack.stadiumhunterapi.model.Stadium;
 import com.ironhack.stadiumhunterapi.model.User;
 import com.ironhack.stadiumhunterapi.service.impl.ReviewService;
 import com.ironhack.stadiumhunterapi.service.impl.StadiumService;
 import com.ironhack.stadiumhunterapi.service.impl.UserService;
+import org.jboss.jandex.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +18,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Currency;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -45,13 +49,36 @@ public class StadiumHunterApiApplication {
 	@Bean
 	CommandLineRunner run(StadiumService stadiumService, ReviewService reviewService, UserService userService) {
 		return args -> {
-			/*stadiumService.saveStadium(new Stadium(1L,"La Bombonera", "Buenos Aires","Argentina",50000,-34.635497,-58.364613));
-			stadiumService.saveStadium(new Stadium(2L,"La Bombonera", "Buenos Aires","Argentina",50000,-34.635497,-58.364613));
-			stadiumService.saveStadium(new Stadium(3L,"Camp Nou", "Barcelona","España",50000,41.381339,2.122745));*/
-
 			userService.saveUser(new User("Pilar","pilar@gmail.com", "P@ssw0rd"));
 
+			ObjectMapper mapper = new ObjectMapper();
+			//InputStream inputStream = new FileInputStream(new File("../../resources/json/stadiums.json"));
+			//TypeReference<List<Stadium>> typeReference = new TypeReference<List<Stadium>>(){};
+			//List<Stadium> stadiums = mapper.readValue(inputStream, typeReference);
+			URL resource = Main.class.getClassLoader().getResource("stadiums.json");
+			byte[] bytes = Files.readAllBytes(Paths.get(resource.toURI()));
+			String json = new String(bytes);
 
+			JsonNode node = mapper.readTree(json);
+			if(node.isArray()){
+				Long id = 1L;
+				for(JsonNode jsonNode : node){
+					String name = jsonNode.get("Name").asText();
+					String city = jsonNode.get("Town").asText();
+					String country = jsonNode.get("Nation").asText();
+					int capacity = 0;
+					try{
+						capacity = Integer.parseInt(jsonNode.get("Capacity").asText().replace(".",""));
+					} catch(Exception e){
+						capacity = 0;
+					}
+					double latitude = jsonNode.get("Latitude").asDouble();
+					double longitude = jsonNode.get("Longitude").asDouble();
+					Stadium stadium = new Stadium(id,name,city, country, capacity, latitude, longitude);
+					stadiumService.saveStadium(stadium);
+					id+=1;
+				}
+			}
 		};
 	}
 
